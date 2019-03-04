285ca3e6bb3e12bbae5475c760922b7908f5c8e1
Alex Ogola <ogollaokumu@gmail.com>
2019-03-04 13:47:00 +0300

    define applications configurations

--- gallery/settings.py
+++ gallery/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'd=u9b^(s(!h68jl263mk@we8$=o4$3m!(o+kce2^97kp6#ujhi'
