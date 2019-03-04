873a2e9d93ed0761860eec54591d4f95db2faa94
Alex Ogola <ogollaokumu@gmail.com>
2019-03-04 13:42:58 +0300

    create the URLconf

---
+++ index/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='Delurk'),
+    path('search', views.search_results, name='search_results'),
+    path('image/<int:image_id>', views.image, name='single_image'),
+    path('mombasa', views.get_mombasa, name='mombasa'),
+    path('nairobi', views.get_nairobi, name='nairobi'),
+    path('architecture', views.architecture, name='architecture'),
+]
+"""
+We first import the project settings where we added the
+template context processor that allows us to load our images.
+
+We also add the static function from django.conf.urls.static.
+
+We configure our app url to register the MEDIA_ROOT route.
+"""
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
